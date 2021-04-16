b3392461f48dae2a0de9b792df6096057918a3c5
mywatchingtv <mywatchingtv@gmail.com>
2021-04-17 01:34:51 +0200

    channel change

--- js/cstv.js
+++ js/cstv.js
@@ -1,14 +1,14 @@
-var csat = document.getElementById("csat").innerText;
+var csat = parseInt(document.getElementById("csat").innerHTML);
 window.addEventListener('wheel', function(event){
  if (event.deltaY < 0){
   console.log('scrolling up');
   csat = csat + 1;
-  document.getElementById("csat").innerText = csat;
+  document.getElementById("csat").innerHTML = csat;
  }
  else if (event.deltaY > 0) {
   console.log('scrolling down');
   csat = csat - 1;
-  document.getElementById("csat").innerText = csat;
+  document.getElementById("csat").innerHTML = csat;
  }
 });
   
